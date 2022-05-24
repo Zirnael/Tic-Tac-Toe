2254a7791546d26c12d9849fd48dd5b4f38b02c2
Zirnael <mr.tostek@gmail.com>
2022-05-24 21:51:18 +0200

    Update cxxProject.cpp

--- cxxProject/cxxProject.cpp
+++ cxxProject/cxxProject.cpp
@@ -2,10 +2,25 @@
 #include <SDL_image.h>
 #include <SDL_timer.h>
 
+void declareWinner(int winner, SDL_Window* win) {
+	switch (winner)
+	{
+	case 0:
+		SDL_SetWindowTitle(win, "Draw");
+		break;
+	case 1:
+		SDL_SetWindowTitle(win, "X wins");
+		break;
+	case 2:
+		SDL_SetWindowTitle(win, "O wins");
+	default:
+		break;
+	}
+}
 int main(int argc, char* argv[])
 {
 
-	SDL_Window* win = SDL_CreateWindow("GAME", // creates a window
+	SDL_Window* win = SDL_CreateWindow("X to move", // creates a window
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
 		600, 600, 0);
@@ -18,25 +33,30 @@ int main(int argc, char* argv[])
 	SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
 
 	// creates a surface to load an image into the main memory
-	SDL_Surface* surface;
+	SDL_Surface* s1;
 	SDL_Surface* s2;
-
+	SDL_Surface* s3;
 	// please provide a path for your image
-	surface = IMG_Load("xLetter.png");
-	s2 = IMG_Load("board.png");
+	s1 = IMG_Load("board.png");
+	s2 = IMG_Load("xLetter.png");
+	s3 = IMG_Load("oLetter.png");
 
 	// loads image to our graphics hardware memory.
-	SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface);
-	SDL_Texture* background = SDL_CreateTextureFromSurface(rend, s2);
+	SDL_Texture* background = SDL_CreateTextureFromSurface(rend, s1);
+	SDL_Texture* xLetter = SDL_CreateTextureFromSurface(rend, s2);
+	SDL_Texture* oLetter = SDL_CreateTextureFromSurface(rend, s3);
 
 
 	// clears main-memory
-	SDL_FreeSurface(surface);
+	SDL_FreeSurface(s1);
+	SDL_FreeSurface(s2);
+	SDL_FreeSurface(s3);
 
 	// let us control our image position
 	// so that we can move it with our keyboard.
 	SDL_Rect dest;
 	SDL_Rect mouse;
+	SDL_Rect position;
 	SDL_Rect origin;
 
 	origin.x = 0;
@@ -44,32 +64,35 @@ int main(int argc, char* argv[])
 	origin.w = 600;
 	origin.h = 600;
 
-	// connects our texture with dest to control position
-	SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h);
-	SDL_QueryTexture(tex, NULL, NULL, &dest.w, &mouse.h);
-
-	// adjust height and width of our image box.
-	dest.w /= 6;
-	dest.h /= 6;
+	position.w = 95;
+	position.h = 170;
 
 	// set background color
 	SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
 
-	// sets initial x-position of object
-	dest.x = (1000 - dest.w) / 2;
-
-	// sets initial y-position of object
-	dest.y = (1000 - dest.h) / 2;
-
 	// State of the board
-	
+
 	int state[3][3];
+	for (size_t i = 0; i < 3; i++)
+	{
+		for (size_t j = 0; j < 3; j++)
+		{
+			state[i][j] = 0;
+		}
+
+	}
 
 	// controls animation loop
 	int close = 0;
 
 	// speed of box
 	int speed = 3000;
+
+	// 1 - X 
+	// 2 - O
+	int turn = 1;
+
+	bool isWinner = false;
 
 	// animation loop
 	while (!close) {
@@ -88,6 +111,64 @@ int main(int argc, char* argv[])
 				mouse.x = event.motion.x;
 				mouse.y = event.motion.y;
 				break;
+
+			case SDL_MOUSEBUTTONDOWN:
+			{
+				int r = event.button.y / 200;
+				int c = event.button.x / 200;
+				if (state[r][c] == 0 && !isWinner) {
+					state[r][c] = turn;
+					turn %= 2;
+					turn++;
+					if (turn == 1)
+						SDL_SetWindowTitle(win, "X to move");
+					if (turn == 2)
+						SDL_SetWindowTitle(win, "O to move");
+					bool isDraw = true;
+					for (size_t i = 0; i < 3; i++)
+					{
+						for (size_t j = 0; j < 3; ++j) {
+							if (state[i][j] == 0) {
+								isDraw = false;
+							}
+						}
+					}
+					if (isDraw) {
+						declareWinner(0, win);
+						isWinner = true;
+					}
+					for (size_t row = 0; row < 3; row++)
+					{
+						int potentialWinner = state[row][0];
+						if (potentialWinner == state[row][1] && potentialWinner == state[row][2] && potentialWinner > 0)
+						{
+							declareWinner(potentialWinner, win);
+							isWinner = true;
+						}
+					}
+					for (size_t col = 0; col < 3; col++)
+					{
+						int potentialWinner = state[0][col];
+						if (potentialWinner == state[1][col] && potentialWinner == state[2][col] && potentialWinner > 0)
+						{
+							declareWinner(potentialWinner, win);
+							isWinner = true;
+						}
+					}
+					int potentialWinner = state[0][0];
+					if (potentialWinner == state[1][1] && potentialWinner == state[2][2] && potentialWinner > 0) {
+
+						declareWinner(potentialWinner, win);
+						isWinner = true;
+					}
+					potentialWinner = state[0][2];
+					if (potentialWinner == state[1][1] && potentialWinner == state[2][0] && potentialWinner > 0) {
+						declareWinner(potentialWinner, win);
+						isWinner = true;
+					}
+				}
+				break;
+			}
 
 			case SDL_KEYDOWN:
 				// keyboard API for key pressed
@@ -108,6 +189,19 @@ int main(int argc, char* argv[])
 				case SDL_SCANCODE_RIGHT:
 					dest.x += speed / 30;
 					break;
+				case SDL_SCANCODE_R:
+
+					for (size_t i = 0; i < 3; i++)
+					{
+						for (size_t j = 0; j < 3; j++)
+						{
+							state[i][j] = 0;
+						}
+					}
+					SDL_SetWindowTitle(win, "X to move");
+					turn = 1;
+					isWinner = false;
+
 				default:
 					break;
 				}
@@ -122,15 +216,19 @@ int main(int argc, char* argv[])
 			for (size_t c = 0; c < 3; c++)
 			{
 				if (state[r][c] == 1) {
-
+					position.x = 50 + 200 * c;
+					position.y = 20 + 200 * r;
+					SDL_RenderCopy(rend, xLetter, NULL, &position);
 				}
 				else if (state[r][c] == 2) {
 
+					position.x = 50 + 200 * c;
+					position.y = 20 + 200 * r;
+					SDL_RenderCopy(rend, oLetter, NULL, &position);
 				}
 			}
 		}
-		SDL_RenderCopy(rend, tex, NULL, &dest);
-		SDL_RenderCopy(rend, tex, NULL, &mouse);
+
 		// triggers the double buffers
 		// for multiple rendering
 		SDL_RenderPresent(rend);
@@ -140,7 +238,10 @@ int main(int argc, char* argv[])
 	}
 
 	// destroy texture
-	SDL_DestroyTexture(tex);
+	SDL_DestroyTexture(xLetter);
+	SDL_DestroyTexture(oLetter);
+	SDL_DestroyTexture(background);
+
 
 	// destroy renderer
 	SDL_DestroyRenderer(rend);
@@ -153,3 +254,4 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
+
